527e5d29d7672c2b84a57316683b61f4e7e243b7
samsmc <samantha.martins@gmail.com>
2020-12-08 13:08:59 +0100

    heroku try again

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const createError = require("http-errors");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
-const User = require("../models/User");
+const User = require("../models/user");
 
 // HELPER FUNCTIONS
 const {
